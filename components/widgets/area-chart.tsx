6b0ebd18f2bf54bff7e74b7b24a7b44dc0781f8e
maishaSupritee <maisha.sup@gmail.com>
2025-02-24 12:27:58 -0800

    Update area-chart for appointments
    Signed-off-by: maishaSupritee <maisha.sup@gmail.com>

--- components/widgets/area-chart.tsx
+++ components/widgets/area-chart.tsx
@@ -4,13 +4,6 @@ import * as React from "react";
 import { Area, AreaChart, CartesianGrid, XAxis } from "recharts";
 
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import {
   ChartConfig,
   ChartContainer,
   ChartLegend,
@@ -18,246 +11,193 @@ import {
   ChartTooltip,
   ChartTooltipContent,
 } from "@/components/ui/chart";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-const chartData = [
-  { date: "2024-04-01", desktop: 222, mobile: 150 },
-  { date: "2024-04-02", desktop: 97, mobile: 180 },
-  { date: "2024-04-03", desktop: 167, mobile: 120 },
-  { date: "2024-04-04", desktop: 242, mobile: 260 },
-  { date: "2024-04-05", desktop: 373, mobile: 290 },
-  { date: "2024-04-06", desktop: 301, mobile: 340 },
-  { date: "2024-04-07", desktop: 245, mobile: 180 },
-  { date: "2024-04-08", desktop: 409, mobile: 320 },
-  { date: "2024-04-09", desktop: 59, mobile: 110 },
-  { date: "2024-04-10", desktop: 261, mobile: 190 },
-  { date: "2024-04-11", desktop: 327, mobile: 350 },
-  { date: "2024-04-12", desktop: 292, mobile: 210 },
-  { date: "2024-04-13", desktop: 342, mobile: 380 },
-  { date: "2024-04-14", desktop: 137, mobile: 220 },
-  { date: "2024-04-15", desktop: 120, mobile: 170 },
-  { date: "2024-04-16", desktop: 138, mobile: 190 },
-  { date: "2024-04-17", desktop: 446, mobile: 360 },
-  { date: "2024-04-18", desktop: 364, mobile: 410 },
-  { date: "2024-04-19", desktop: 243, mobile: 180 },
-  { date: "2024-04-20", desktop: 89, mobile: 150 },
-  { date: "2024-04-21", desktop: 137, mobile: 200 },
-  { date: "2024-04-22", desktop: 224, mobile: 170 },
-  { date: "2024-04-23", desktop: 138, mobile: 230 },
-  { date: "2024-04-24", desktop: 387, mobile: 290 },
-  { date: "2024-04-25", desktop: 215, mobile: 250 },
-  { date: "2024-04-26", desktop: 75, mobile: 130 },
-  { date: "2024-04-27", desktop: 383, mobile: 420 },
-  { date: "2024-04-28", desktop: 122, mobile: 180 },
-  { date: "2024-04-29", desktop: 315, mobile: 240 },
-  { date: "2024-04-30", desktop: 454, mobile: 380 },
-  { date: "2024-05-01", desktop: 165, mobile: 220 },
-  { date: "2024-05-02", desktop: 293, mobile: 310 },
-  { date: "2024-05-03", desktop: 247, mobile: 190 },
-  { date: "2024-05-04", desktop: 385, mobile: 420 },
-  { date: "2024-05-05", desktop: 481, mobile: 390 },
-  { date: "2024-05-06", desktop: 498, mobile: 520 },
-  { date: "2024-05-07", desktop: 388, mobile: 300 },
-  { date: "2024-05-08", desktop: 149, mobile: 210 },
-  { date: "2024-05-09", desktop: 227, mobile: 180 },
-  { date: "2024-05-10", desktop: 293, mobile: 330 },
-  { date: "2024-05-11", desktop: 335, mobile: 270 },
-  { date: "2024-05-12", desktop: 197, mobile: 240 },
-  { date: "2024-05-13", desktop: 197, mobile: 160 },
-  { date: "2024-05-14", desktop: 448, mobile: 490 },
-  { date: "2024-05-15", desktop: 473, mobile: 380 },
-  { date: "2024-05-16", desktop: 338, mobile: 400 },
-  { date: "2024-05-17", desktop: 499, mobile: 420 },
-  { date: "2024-05-18", desktop: 315, mobile: 350 },
-  { date: "2024-05-19", desktop: 235, mobile: 180 },
-  { date: "2024-05-20", desktop: 177, mobile: 230 },
-  { date: "2024-05-21", desktop: 82, mobile: 140 },
-  { date: "2024-05-22", desktop: 81, mobile: 120 },
-  { date: "2024-05-23", desktop: 252, mobile: 290 },
-  { date: "2024-05-24", desktop: 294, mobile: 220 },
-  { date: "2024-05-25", desktop: 201, mobile: 250 },
-  { date: "2024-05-26", desktop: 213, mobile: 170 },
-  { date: "2024-05-27", desktop: 420, mobile: 460 },
-  { date: "2024-05-28", desktop: 233, mobile: 190 },
-  { date: "2024-05-29", desktop: 78, mobile: 130 },
-  { date: "2024-05-30", desktop: 340, mobile: 280 },
-  { date: "2024-05-31", desktop: 178, mobile: 230 },
-  { date: "2024-06-01", desktop: 178, mobile: 200 },
-  { date: "2024-06-02", desktop: 470, mobile: 410 },
-  { date: "2024-06-03", desktop: 103, mobile: 160 },
-  { date: "2024-06-04", desktop: 439, mobile: 380 },
-  { date: "2024-06-05", desktop: 88, mobile: 140 },
-  { date: "2024-06-06", desktop: 294, mobile: 250 },
-  { date: "2024-06-07", desktop: 323, mobile: 370 },
-  { date: "2024-06-08", desktop: 385, mobile: 320 },
-  { date: "2024-06-09", desktop: 438, mobile: 480 },
-  { date: "2024-06-10", desktop: 155, mobile: 200 },
-  { date: "2024-06-11", desktop: 92, mobile: 150 },
-  { date: "2024-06-12", desktop: 492, mobile: 420 },
-  { date: "2024-06-13", desktop: 81, mobile: 130 },
-  { date: "2024-06-14", desktop: 426, mobile: 380 },
-  { date: "2024-06-15", desktop: 307, mobile: 350 },
-  { date: "2024-06-16", desktop: 371, mobile: 310 },
-  { date: "2024-06-17", desktop: 475, mobile: 520 },
-  { date: "2024-06-18", desktop: 107, mobile: 170 },
-  { date: "2024-06-19", desktop: 341, mobile: 290 },
-  { date: "2024-06-20", desktop: 408, mobile: 450 },
-  { date: "2024-06-21", desktop: 169, mobile: 210 },
-  { date: "2024-06-22", desktop: 317, mobile: 270 },
-  { date: "2024-06-23", desktop: 480, mobile: 530 },
-  { date: "2024-06-24", desktop: 132, mobile: 180 },
-  { date: "2024-06-25", desktop: 141, mobile: 190 },
-  { date: "2024-06-26", desktop: 434, mobile: 380 },
-  { date: "2024-06-27", desktop: 448, mobile: 490 },
-  { date: "2024-06-28", desktop: 149, mobile: 200 },
-  { date: "2024-06-29", desktop: 103, mobile: 160 },
-  { date: "2024-06-30", desktop: 446, mobile: 400 },
-];
 
 const chartConfig = {
-  visitors: {
-    label: "Visitors",
-  },
-  desktop: {
-    label: "Desktop",
-    color: "hsl(var(--chart-1))",
-  },
-  mobile: {
-    label: "Mobile",
+  booked: {
+    label: "Booked",
     color: "hsl(var(--chart-2))",
+  },
+  transferred: {
+    label: "Transferred",
+    color: "hsl(var(--chart-1))",
   },
 } satisfies ChartConfig;
 
-export function AppointmentsAreaChart() {
-  const [timeRange, setTimeRange] = React.useState("90d");
+//sample data
+const chartData = [
+  { date: "2024-04-01", booked: 222, transferred: 150 },
+  { date: "2024-04-02", booked: 97, transferred: 180 },
+  { date: "2024-04-03", booked: 167, transferred: 120 },
+  { date: "2024-04-04", booked: 242, transferred: 260 },
+  { date: "2024-04-05", booked: 373, transferred: 290 },
+  { date: "2024-04-06", booked: 301, transferred: 340 },
+  { date: "2024-04-07", booked: 245, transferred: 180 },
+  { date: "2024-04-08", booked: 409, transferred: 320 },
+  { date: "2024-04-09", booked: 59, transferred: 110 },
+  { date: "2024-04-10", booked: 261, transferred: 190 },
+  { date: "2024-04-11", booked: 327, transferred: 350 },
+  { date: "2024-04-12", booked: 292, transferred: 210 },
+  { date: "2024-04-13", booked: 342, transferred: 380 },
+  { date: "2024-04-14", booked: 137, transferred: 220 },
+  { date: "2024-04-15", booked: 120, transferred: 170 },
+  { date: "2024-04-16", booked: 138, transferred: 190 },
+  { date: "2024-04-17", booked: 446, transferred: 360 },
+  { date: "2024-04-18", booked: 364, transferred: 410 },
+  { date: "2024-04-19", booked: 243, transferred: 180 },
+  { date: "2024-04-20", booked: 89, transferred: 150 },
+  { date: "2024-04-21", booked: 137, transferred: 200 },
+  { date: "2024-04-22", booked: 224, transferred: 170 },
+  { date: "2024-04-23", booked: 138, transferred: 230 },
+  { date: "2024-04-24", booked: 387, transferred: 290 },
+  { date: "2024-04-25", booked: 215, transferred: 250 },
+  { date: "2024-04-26", booked: 75, transferred: 130 },
+  { date: "2024-04-27", booked: 383, transferred: 420 },
+  { date: "2024-04-28", booked: 122, transferred: 180 },
+  { date: "2024-04-29", booked: 315, transferred: 240 },
+  { date: "2024-04-30", booked: 454, transferred: 380 },
+  { date: "2024-05-01", booked: 165, transferred: 220 },
+  { date: "2024-05-02", booked: 293, transferred: 310 },
+  { date: "2024-05-03", booked: 247, transferred: 190 },
+  { date: "2024-05-04", booked: 385, transferred: 420 },
+  { date: "2024-05-05", booked: 481, transferred: 390 },
+  { date: "2024-05-06", booked: 498, transferred: 520 },
+  { date: "2024-05-07", booked: 388, transferred: 300 },
+  { date: "2024-05-08", booked: 149, transferred: 210 },
+  { date: "2024-05-09", booked: 227, transferred: 180 },
+  { date: "2024-05-10", booked: 293, transferred: 330 },
+  { date: "2024-05-11", booked: 335, transferred: 270 },
+  { date: "2024-05-12", booked: 197, transferred: 240 },
+  { date: "2024-05-13", booked: 197, transferred: 160 },
+  { date: "2024-05-14", booked: 448, transferred: 490 },
+  { date: "2024-05-15", booked: 473, transferred: 380 },
+  { date: "2024-05-16", booked: 338, transferred: 400 },
+  { date: "2024-05-17", booked: 499, transferred: 420 },
+  { date: "2024-05-18", booked: 315, transferred: 350 },
+  { date: "2024-05-19", booked: 235, transferred: 180 },
+  { date: "2024-05-20", booked: 177, transferred: 230 },
+  { date: "2024-05-21", booked: 82, transferred: 140 },
+  { date: "2024-05-22", booked: 81, transferred: 120 },
+  { date: "2024-05-23", booked: 252, transferred: 290 },
+  { date: "2024-05-24", booked: 294, transferred: 220 },
+  { date: "2024-05-25", booked: 201, transferred: 250 },
+  { date: "2024-05-26", booked: 213, transferred: 170 },
+  { date: "2024-05-27", booked: 420, transferred: 460 },
+  { date: "2024-05-28", booked: 233, transferred: 190 },
+  { date: "2024-05-29", booked: 78, transferred: 130 },
+  { date: "2024-05-30", booked: 340, transferred: 280 },
+  { date: "2024-05-31", booked: 178, transferred: 230 },
+  { date: "2024-06-01", booked: 178, transferred: 200 },
+  { date: "2024-06-02", booked: 470, transferred: 410 },
+  { date: "2024-06-03", booked: 103, transferred: 160 },
+  { date: "2024-06-04", booked: 439, transferred: 380 },
+  { date: "2024-06-05", booked: 88, transferred: 140 },
+  { date: "2024-06-06", booked: 294, transferred: 250 },
+  { date: "2024-06-07", booked: 323, transferred: 370 },
+  { date: "2024-06-08", booked: 385, transferred: 320 },
+  { date: "2024-06-09", booked: 438, transferred: 480 },
+  { date: "2024-06-10", booked: 155, transferred: 200 },
+  { date: "2024-06-11", booked: 92, transferred: 150 },
+  { date: "2024-06-12", booked: 492, transferred: 420 },
+  { date: "2024-06-13", booked: 81, transferred: 130 },
+  { date: "2024-06-14", booked: 426, transferred: 380 },
+  { date: "2024-06-15", booked: 307, transferred: 350 },
+  { date: "2024-06-16", booked: 371, transferred: 310 },
+  { date: "2024-06-17", booked: 475, transferred: 520 },
+  { date: "2024-06-18", booked: 107, transferred: 170 },
+  { date: "2024-06-19", booked: 341, transferred: 290 },
+  { date: "2024-06-20", booked: 408, transferred: 450 },
+  { date: "2024-06-21", booked: 169, transferred: 210 },
+  { date: "2024-06-22", booked: 317, transferred: 270 },
+  { date: "2024-06-23", booked: 480, transferred: 530 },
+  { date: "2024-06-24", booked: 132, transferred: 180 },
+  { date: "2024-06-25", booked: 141, transferred: 190 },
+  { date: "2024-06-26", booked: 434, transferred: 380 },
+  { date: "2024-06-27", booked: 448, transferred: 490 },
+  { date: "2024-06-28", booked: 149, transferred: 200 },
+  { date: "2024-06-29", booked: 103, transferred: 160 },
+  { date: "2024-06-30", booked: 446, transferred: 400 },
+];
 
-  const filteredData = chartData.filter((item) => {
-    const date = new Date(item.date);
-    const referenceDate = new Date("2024-06-30");
-    let daysToSubtract = 90;
-    if (timeRange === "30d") {
-      daysToSubtract = 30;
-    } else if (timeRange === "7d") {
-      daysToSubtract = 7;
-    }
-    const startDate = new Date(referenceDate);
-    startDate.setDate(startDate.getDate() - daysToSubtract);
-    return date >= startDate;
-  });
-
+export function AppointmentsAreaChart({ data = chartData }) {
   return (
-    <Card>
-      <CardHeader className="flex items-center gap-2 space-y-0 border-b py-5 sm:flex-row">
-        <div className="grid flex-1 gap-1 text-center sm:text-left">
-          <CardTitle>Area Chart - Interactive</CardTitle>
-          <CardDescription>
-            Showing total visitors for the last 3 months
-          </CardDescription>
-        </div>
-        <Select value={timeRange} onValueChange={setTimeRange}>
-          <SelectTrigger
-            className="w-[160px] rounded-lg sm:ml-auto"
-            aria-label="Select a value"
-          >
-            <SelectValue placeholder="Last 3 months" />
-          </SelectTrigger>
-          <SelectContent className="rounded-xl">
-            <SelectItem value="90d" className="rounded-lg">
-              Last 3 months
-            </SelectItem>
-            <SelectItem value="30d" className="rounded-lg">
-              Last 30 days
-            </SelectItem>
-            <SelectItem value="7d" className="rounded-lg">
-              Last 7 days
-            </SelectItem>
-          </SelectContent>
-        </Select>
-      </CardHeader>
-      <CardContent className="px-2 pt-4 sm:px-6 sm:pt-6">
-        <ChartContainer
-          config={chartConfig}
-          className="aspect-auto h-[250px] w-full"
-        >
-          <AreaChart data={filteredData}>
-            <defs>
-              <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
-                <stop
-                  offset="5%"
-                  stopColor="var(--color-desktop)"
-                  stopOpacity={0.8}
-                />
-                <stop
-                  offset="95%"
-                  stopColor="var(--color-desktop)"
-                  stopOpacity={0.1}
-                />
-              </linearGradient>
-              <linearGradient id="fillMobile" x1="0" y1="0" x2="0" y2="1">
-                <stop
-                  offset="5%"
-                  stopColor="var(--color-mobile)"
-                  stopOpacity={0.8}
-                />
-                <stop
-                  offset="95%"
-                  stopColor="var(--color-mobile)"
-                  stopOpacity={0.1}
-                />
-              </linearGradient>
-            </defs>
-            <CartesianGrid vertical={false} />
-            <XAxis
-              dataKey="date"
-              tickLine={false}
-              axisLine={false}
-              tickMargin={8}
-              minTickGap={32}
-              tickFormatter={(value) => {
-                const date = new Date(value);
-                return date.toLocaleDateString("en-US", {
+    <ChartContainer
+      config={chartConfig}
+      className="aspect-auto h-[250px] w-full"
+    >
+      <AreaChart data={data}>
+        <defs>
+          <linearGradient id="fillBooked" x1="0" y1="0" x2="0" y2="1">
+            <stop
+              offset="5%"
+              stopColor={chartConfig.booked.color}
+              stopOpacity={0.8}
+            />
+            <stop
+              offset="95%"
+              stopColor={chartConfig.booked.color}
+              stopOpacity={0.1}
+            />
+          </linearGradient>
+          <linearGradient id="fillTransferred" x1="0" y1="0" x2="0" y2="1">
+            <stop
+              offset="5%"
+              stopColor={chartConfig.transferred.color}
+              stopOpacity={0.8}
+            />
+            <stop
+              offset="95%"
+              stopColor={chartConfig.transferred.color}
+              stopOpacity={0.1}
+            />
+          </linearGradient>
+        </defs>
+        <CartesianGrid vertical={false} />
+        <XAxis
+          dataKey="date"
+          tickLine={false}
+          axisLine={false}
+          tickMargin={8}
+          minTickGap={32}
+          tickFormatter={(value) => {
+            const date = new Date(value);
+            return date.toLocaleDateString("en-US", {
+              month: "short",
+              day: "numeric",
+            });
+          }}
+        />
+        <ChartTooltip
+          cursor={false}
+          content={
+            <ChartTooltipContent
+              labelFormatter={(value) => {
+                return new Date(value).toLocaleDateString("en-US", {
                   month: "short",
                   day: "numeric",
                 });
               }}
+              indicator="dot"
             />
-            <ChartTooltip
-              cursor={false}
-              content={
-                <ChartTooltipContent
-                  labelFormatter={(value) => {
-                    return new Date(value).toLocaleDateString("en-US", {
-                      month: "short",
-                      day: "numeric",
-                    });
-                  }}
-                  indicator="dot"
-                />
-              }
-            />
-            <Area
-              dataKey="mobile"
-              type="natural"
-              fill="url(#fillMobile)"
-              stroke="var(--color-mobile)"
-              stackId="a"
-            />
-            <Area
-              dataKey="desktop"
-              type="natural"
-              fill="url(#fillDesktop)"
-              stroke="var(--color-desktop)"
-              stackId="a"
-            />
-            <ChartLegend content={<ChartLegendContent />} />
-          </AreaChart>
-        </ChartContainer>
-      </CardContent>
-    </Card>
+          }
+        />
+        <Area
+          dataKey="transferred"
+          type="natural"
+          fill="url(#fillTransferred)"
+          stroke={chartConfig.transferred.color}
+          stackId="a"
+          name="Transferred"
+        />
+        <Area
+          dataKey="booked"
+          type="natural"
+          fill="url(#fillBooked)"
+          stroke={chartConfig.booked.color}
+          stackId="a"
+          name="Booked"
+        />
+        <ChartLegend content={<ChartLegendContent />} />
+      </AreaChart>
+    </ChartContainer>
   );
 }
