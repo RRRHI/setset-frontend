3d68175c2ed4bd836630b0a8782923b1f36e8a55
worifung achu <achuclinton47@gmail.com>
2025-03-27 18:44:40 -0700

    .

--- components/widgets/sidebar/setting-sidebar.tsx
+++ components/widgets/sidebar/setting-sidebar.tsx
@@ -37,18 +37,19 @@ export function SettingSidebar({
         </SidebarGroupLabel>
         <SidebarGroupContent className="my-5">
         {menuItems.map((item) => (
-          <SidebarMenuItem
-            key={item.url}
-            onClick={() => handleScrollToSection(item.url)}
-            className={`mx-2 flex h-12 cursor-default list-none items-center justify-start gap-1 rounded-xl p-[30px] transition-transform duration-300 ease-in-out hover:translate-x-4 relative ${currView === item.url ? "translate-x-4 [&>span>svg]:stroke-accent [&>span]:bg-primary-foreground [&>span]:text-[38px] [&>span]:text-primary-foreground [&>div]:w-2 [&>div]:bg-primary-foreground" : "bg-transparent"}`}
-          >
-            <div className="absolute left-0 h-[38px] w-2 rounded-xl self-center mx-auto my-auto"></div>
-
-            <span className="flex size-[38px] items-center justify-center rounded-xl">
-             {item.icon}
-            </span>
-            <p className="ml-2">{item.title}</p>
-          </SidebarMenuItem>
+             <SidebarMenuItem
+             key={item.url}
+             onClick={() => handleScrollToSection(item.url)}
+             className={`mx-2 flex h-12 cursor-default list-none items-center justify-start gap-1 rounded-xl p-[30px] transition-transform duration-300 ease-in-out hover:translate-x-4 relative ${currView === item.url ? "[&>span>svg]:stroke-accent [&>span]:bg-primary-foreground [&>span]:text-[38px] [&>span]:text-primary-foreground [&>div]:w-2 [&>div]:bg-primary-foreground bg-foreground text-primary" : "bg-transparent"}`}
+           >
+             <div className="absolute left-0 h-[38px] w-2 rounded-xl self-center mx-auto my-auto"></div>
+ 
+             <span className="flex size-[38px] items-center justify-center rounded-xl">
+              {item.icon}
+             </span>
+             <p className="ml-2">{item.title}</p>
+           </SidebarMenuItem>
+         
         ))}
         </SidebarGroupContent>
         </SidebarGroup>
